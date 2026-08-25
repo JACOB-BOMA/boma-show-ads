4958ecc84edae9e9ff6ad8a946072615a4fcf252
JACOB-BOMA <jwilder@bomageorgia.org>
2026-08-25 12:50:14 -0400

    Define ad objects for homepage and leaderboard
    Added ad configurations for homepage and leaderboard.

--- ads.js
+++ ads.js
@@ -1,1 +1,15 @@
+const ads = {
 
+  "homepage-square": {
+    image: "images/square.jpg",
+    url: "https://www.example.com",
+    alt: "Homepage Sponsor"
+  },
+
+  "leaderboard": {
+    image: "images/leaderboard.jpg",
+    url: "https://www.example.com",
+    alt: "Leaderboard Sponsor"
+  }
+
+};

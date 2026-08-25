a3a01bcd6ecbb99fc5d36c13b0ece27471597f4b
JACOB-BOMA <jwilder@bomageorgia.org>
2026-08-25 13:05:26 -0400

    Update ad URLs to new exhibit hall links

--- ads.js
+++ ads.js
@@ -2,31 +2,31 @@ const ads = {
 
   "ad-place1": {
     image: "images/ad-place1.png",
-    url: "https://www.example.com",
+    url: "https://portal.bomageorgia.org/exhibits/exhibit-hall-booths/?HallId=66422f6b-8784-4f74-a9a6-2b3fa43b527a",
     alt: "Advertisement 1"
   },
 
   "ad-place2": {
     image: "images/ad-place2.png",
-    url: "https://www.example.com",
+    url: "https://portal.bomageorgia.org/exhibits/exhibit-hall-booths/?HallId=66422f6b-8784-4f74-a9a6-2b3fa43b527a",
     alt: "Advertisement 2"
   },
 
   "ad-place3": {
     image: "images/ad-place3.png",
-    url: "https://www.example.com",
+    url: "https://portal.bomageorgia.org/exhibits/exhibit-hall-booths/?HallId=66422f6b-8784-4f74-a9a6-2b3fa43b527a",
     alt: "Advertisement 3"
   },
 
   "ad-place4": {
     image: "images/ad-place4.png",
-    url: "https://www.example.com",
+    url: "https://portal.bomageorgia.org/exhibits/exhibit-hall-booths/?HallId=66422f6b-8784-4f74-a9a6-2b3fa43b527a",
     alt: "Advertisement 4"
   },
 
   "ad-place-leader": {
     image: "images/ad-place_leader.png",
-    url: "https://www.example.com",
+    url: "https://portal.bomageorgia.org/exhibits/exhibit-hall-booths/?HallId=66422f6b-8784-4f74-a9a6-2b3fa43b527a",
     alt: "Leaderboard Advertisement"
   }
 
